20a6d1937b7fadbe5043f3b62ec1d14b96c17cbc
weirshi <woshiweir@yahoo.com>
2020-10-19 18:50:09 +0800

    更新结构

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+const config = {
+    port: 9000,
+    database: {
+        HOST: "localhost",
+        USER: "root",
+        PORT: "3306",
+        PASSWORD:"loveMCY1314",
+        DATABASE: process.env.NODE_ENV === "development" ? "blogdev" : "blog"
+    }
+};
+
+module.exports = config;
